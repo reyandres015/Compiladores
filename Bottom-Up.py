f9b8665b23a276c743d7a2e5549249b0f24ae1d8
Nicolas Betancur <nicobetancurochoa@gmail.com>
2023-11-21 16:50:39 -0500

    Bottom up

--- Bottom-Up.py
+++ Bottom-Up.py
@@ -10,18 +10,7 @@ class Bottom_up:
         self.NoTerminals=NoTerminals
         self.cadenas=cadenas
         self.Terminals=set()
-        self.Resultados=[]
-        self.terminals()
-        self.Terminals=list(self.Terminals)
-        # Suponiendo que self.NoTerminals y self.Terminals son listas de strings
-        self.TableM = [["" for _ in range(len(self.Terminals))] for _ in range(len(self.NoTerminals))]
-        self.dicRows={}
-        self.dicColumns={}
-        self.Error=False
-        self.dicAsing()
-        self.predictiveParsingTable()
-        for cadena in self.cadenas:
-            self.predictiveParsing(cadena)
+      
 
 
     def extenderGramatica(self, grammar):
